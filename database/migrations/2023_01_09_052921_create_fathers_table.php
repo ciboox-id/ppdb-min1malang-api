9b25acdacd8b5a66d3498a5565bdf60f1eeb087b
kkafi09 <afi.firda@gmail.com>
2023-01-11 08:05:51 +0700

    add: first commit

---
+++ database/migrations/2023_01_09_052921_create_fathers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fathers', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_lengkap_ayah')->nullable();
+            $table->string('nik_ayah')->nullable();
+            $table->text('pekerjaan_ayah')->nullable();
+            $table->text('nama_kantor_ayah')->nullable();
+            $table->string('penghasilan_ayah')->nullable();
+            $table->string('no_telp')->nullable();
+            $table->foreignId('id_user');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fathers');
+    }
+};
