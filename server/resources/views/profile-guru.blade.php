15e985809717e45806270483519635c7d7a82848
kkafi09 <afi.firda@gmail.com>
2023-02-17 13:23:54 +0700

    fix: bug design

--- server/resources/views/profile-guru.blade.php
+++ server/resources/views/profile-guru.blade.php
@@ -70,7 +70,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel">Tambah Prestasi</h5>
+                    <h5 class="modal-title" id="staticBackdropLabel">Tambah Data Panitia</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
